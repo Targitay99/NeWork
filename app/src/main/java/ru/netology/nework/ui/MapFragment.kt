8e0de866b5d57da0e2430255a4cd76cec7412a9b
Targitay99 <olin@mail.ru>
2023-08-10 22:05:54 +0400

    User Location

--- app/src/main/java/ru/netology/nework/ui/MapFragment.kt
+++ app/src/main/java/ru/netology/nework/ui/MapFragment.kt
@@ -1,12 +1,20 @@
 package ru.netology.nework.ui
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
 import android.opengl.Visibility
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.google.android.gms.location.LocationServices
 import ru.netology.nework.R
 import ru.netology.nework.databinding.FragmentMapBinding
 import ru.netology.nework.utils.Companion.Companion.idPoint
@@ -17,15 +25,30 @@ import com.yandex.mapkit.Animation
 import com.yandex.mapkit.MapKitFactory
 import com.yandex.mapkit.geometry.Point
 import com.yandex.mapkit.map.CameraPosition
-import dagger.hilt.android.AndroidEntryPoint
+import com.yandex.mapkit.map.InputListener
+import com.yandex.mapkit.map.Map
+import com.yandex.mapkit.user_location.UserLocationLayer
 import ru.netology.nework.utils.Companion.Companion.latitudeNew
 import ru.netology.nework.utils.Companion.Companion.longitudeNew
 
 
-@AndroidEntryPoint
-class MapsFragment : Fragment() {
+
+class MapsFragment : Fragment() , LocationListener, InputListener {
 
     private lateinit var binding: FragmentMapBinding
+    private lateinit var userLocationLayer: UserLocationLayer
+    private val requestPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            if (isGranted) {
+                binding.mapview.apply {
+                    userLocationLayer.isVisible = true
+                    userLocationLayer.isHeadingEnabled = false
+                }
+            } else {
+                Toast.makeText(context, R.string.no_location, Toast.LENGTH_LONG)
+                    .show()
+            }
+        }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -33,6 +56,7 @@ class MapsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentMapBinding.inflate(layoutInflater)
+        checkPermission()
         val map = binding.mapview.map
         val latitudeMap = arguments?.latitude ?: 59.945933
         val longitudeMap = arguments?.longitude ?: 30.320045
@@ -42,6 +66,7 @@ class MapsFragment : Fragment() {
             Animation(Animation.Type.SMOOTH, 5F),
             null
         )
+        userLocationLayer = MapKitFactory.getInstance().createUserLocationLayer(binding.mapview.mapWindow)
 
         val open = arguments?.getString("open")
         if (open == "newEvent") {
@@ -61,12 +86,51 @@ class MapsFragment : Fragment() {
                 )
                 true
             }
+
+            binding.buttonLocation.setOnClickListener {
+                try {
+                    moveCamera(userLocationLayer.cameraPosition()?.target!!)
+                } catch (e: Exception) {
+                    Toast.makeText(context, R.string.retry, Toast.LENGTH_SHORT)
+                        .show()
+                }
+            }
         }
+
+
         return binding.root
 
 
     }
 
+    private fun moveCamera(point: Point) {
+        binding.mapview.map.move(CameraPosition(point, 10F, 0F, 0F))
+    }
+
+    private fun checkPermission() {
+        when (PackageManager.PERMISSION_GRANTED) {
+            ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ),
+            -> {
+                binding.mapview.apply {
+                    userLocationLayer.isVisible = true
+                    userLocationLayer.isHeadingEnabled = false
+                }
+
+                val fusedLocationProviderClient = LocationServices
+                    .getFusedLocationProviderClient(requireActivity())
+
+                fusedLocationProviderClient.lastLocation.addOnSuccessListener {
+                    println(it)
+                }
+            }
+            else -> {
+                requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+            }
+        }
+    }
 
     override fun onStart() {
         super.onStart()
@@ -78,6 +142,18 @@ class MapsFragment : Fragment() {
         binding.mapview.onStop()
         MapKitFactory.getInstance().onStop()
         super.onStop()
+    }
+
+    override fun onLocationChanged(location: Location) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onMapTap(p0: Map, p1: Point) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onMapLongTap(p0: Map, p1: Point) {
+        TODO("Not yet implemented")
     }
 
 }
